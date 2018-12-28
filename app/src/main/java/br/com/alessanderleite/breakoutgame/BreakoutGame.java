f5f810985c7e2639a9f9312a8137019d2cfe7b41
alessanderleite <alessander.logistica@gmail.com>
2018-12-28 21:13:52 -0200

    initial commit

---
+++ app/src/main/java/br/com/alessanderleite/breakoutgame/BreakoutGame.java
@@ -0,0 +1,12 @@
+package br.com.alessanderleite.breakoutgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BreakoutGame extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
