9227e9747a78c089dfeab04221c5589544cc598f
alessanderleite <alessander.logistica@gmail.com>
2018-12-29 09:13:46 -0200

    implementation of GameView as an inner class

--- app/src/main/java/br/com/alessanderleite/breakoutgame/BreakoutGame.java
+++ app/src/main/java/br/com/alessanderleite/breakoutgame/BreakoutGame.java
@@ -1,12 +1,39 @@
 package br.com.alessanderleite.breakoutgame;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.SurfaceView;
 
 public class BreakoutGame extends Activity {
+
+    // gameView will be the view of the game
+    // It will also hold the logic of the game
+    // and respond to screen touches as well
+    BreakoutView breakoutView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    // Here is our implementation of GameView
+    // It is an inner class.
+    // Note how the final closing curly brace }
+    // is inside SimpleGameEngine
+
+    // Notice we implement runnable so we have
+    // A thread and can override the run method.
+    class BreakoutView extends SurfaceView implements Runnable {
+
+        public BreakoutView(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 }
