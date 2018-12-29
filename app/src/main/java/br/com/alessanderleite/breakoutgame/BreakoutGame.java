40fcf739b91b89bf8b1c5ac84e64833405dc8f7f
alessanderleite <alessander.logistica@gmail.com>
2018-12-29 09:23:43 -0200

    coding the BreakoutView constructor

--- app/src/main/java/br/com/alessanderleite/breakoutgame/BreakoutGame.java
+++ app/src/main/java/br/com/alessanderleite/breakoutgame/BreakoutGame.java
@@ -2,7 +2,10 @@ package br.com.alessanderleite.breakoutgame;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class BreakoutGame extends Activity {
@@ -16,6 +19,9 @@ public class BreakoutGame extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Initialize gameView and set it as the view
+        breakoutView = new BreakoutView(this);
+        setContentView(breakoutView);
     }
 
     // Here is our implementation of GameView
@@ -27,8 +33,42 @@ public class BreakoutGame extends Activity {
     // A thread and can override the run method.
     class BreakoutView extends SurfaceView implements Runnable {
 
+        // This is our thread
+        Thread gameThread = null;
+
+        // This is new. We need a SurfaceHolder
+        // When we use Paint and Canvas in a thread
+        // We will see it in action in the draw method soon.
+        SurfaceHolder ourHolder;
+
+        // A boolean which we will set and unset
+        // when the game is running- or not.
+        volatile boolean playing;
+
+        // Game is paued at the start
+        boolean paused = true;
+
+        // A Canvas and a Paint object
+        Canvas canvas;
+        Paint paint;
+
+        // This variable tracks the game frame rate
+        long fps;
+
+        // This is used to help calculate the fps
+        private long timeThisFrame;
+
+        // When the we initialize (call new()) on gameView
+        // This special constructor method runs
         public BreakoutView(Context context) {
+            // The next line of code asks the
+            // SurfaceView class to set up our object.
+            // How kind.
             super(context);
+
+            // Initialize ourHolder and paint objects
+            ourHolder = getHolder();
+            paint = new Paint();
         }
 
         @Override
