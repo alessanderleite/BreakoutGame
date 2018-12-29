79ca32f4ac39fb4208df8d6ba4e261fee708340c
alessanderleite <alessander.logistica@gmail.com>
2018-12-29 11:53:55 -0200

    Initialize the RectF object and the variables in the Paddle class

--- app/src/main/java/br/com/alessanderleite/breakoutgame/Paddle.java
+++ app/src/main/java/br/com/alessanderleite/breakoutgame/Paddle.java
@@ -1,4 +1,30 @@
 package br.com.alessanderleite.breakoutgame;
 
+import android.graphics.RectF;
+
 public class Paddle {
+
+    // RectF is an object that holds four coordinates - just what we need
+    private RectF rect;
+
+    // How long and high our paddle will be
+    private float length;
+    private float height;
+
+    // X is the far left of the rectangle which forms our paddle
+    private float x;
+
+    // Y is the top coordinate
+    private float y;
+
+    // This will hold the pixels per second speed that the paddle will move
+    private float paddleSpeed;
+
+    // Which ways can the paddle move
+    public final int STOPPED = 0;
+    public final int LEFT = 1;
+    public final int RIGHT = 2;
+
+    // Is the paddle moving and in which direction
+    private int paddleMoving = STOPPED;
 }
