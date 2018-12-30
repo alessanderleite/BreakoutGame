6e2c805556196ef4d3e3e99f24d8f779c05d6bc9
alessanderleite <alessander.logistica@gmail.com>
2018-12-30 18:13:20 -0200

    declare the RectF object and variables in the Ball class

--- app/src/main/java/br/com/alessanderleite/breakoutgame/Ball.java
+++ app/src/main/java/br/com/alessanderleite/breakoutgame/Ball.java
@@ -1,4 +1,14 @@
 package br.com.alessanderleite.breakoutgame;
 
+import android.graphics.RectF;
+
 public class Ball {
+    
+    RectF rect;
+    float xVelocity;
+    float yVelocity;
+    float ballWidth = 10;
+    float ballHeight = 10;
+
+
 }
