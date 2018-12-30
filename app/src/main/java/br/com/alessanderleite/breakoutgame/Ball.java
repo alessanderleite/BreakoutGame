9edbf32d1dfcdab1c87ea63fe665a0eb1c74b85e
alessanderleite <alessander.logistica@gmail.com>
2018-12-30 18:18:30 -0200

    declare the Ball constructor and initialize the variables inside it

--- app/src/main/java/br/com/alessanderleite/breakoutgame/Ball.java
+++ app/src/main/java/br/com/alessanderleite/breakoutgame/Ball.java
@@ -3,12 +3,22 @@ package br.com.alessanderleite.breakoutgame;
 import android.graphics.RectF;
 
 public class Ball {
-    
+
     RectF rect;
     float xVelocity;
     float yVelocity;
     float ballWidth = 10;
     float ballHeight = 10;
 
+    public Ball(int screenX, int screenY) {
+
+        // Start the ball travelling straight up at 100 pixels per second
+        xVelocity = 200;
+        yVelocity = -400;
+
+        // Place the ball in the centre of the screen at the bottom
+        // Make it a 10 pixel x 10 pixel square
+        rect = new RectF();
+    }
 
 }
